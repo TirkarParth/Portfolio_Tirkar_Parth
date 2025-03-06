5830e39a9baffbfadfc675f3436a9abe247f5a0b
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2025-03-07 00:15:26 +0100

    Implemented language switcher with dynamic text and download functionality
    This commit introduces a language switcher for the website, allowing users to toggle between English and German.
    
    1. **Language Switcher**:
       - The language toggle buttons are styled without borders, providing a clean and minimal interface for the user.
       - Clicking on either the "English" or "Deutsch" button dynamically updates the text content on the page and adjusts the download link accordingly.
    
    2. **Dynamic Resume Link**:
       - When the language is switched, the download resume link (`<a>` tag) dynamically updates based on the selected language (English or German).
       - The default resume is set in English, and the German resume link is loaded when switching to German.
    
    3. **CSS & JavaScript Enhancements**:
       - A CSS class was introduced to style the language buttons without a bordered box, keeping the design simple and user-friendly.
       - JavaScript functions manage the language switch and update the content and download links accordingly.
    
    This update enhances user experience by offering multi-language support with seamless language-based content switching.

---
+++ js/language-toggle.js
@@ -0,0 +1,222 @@
+const translations = {
+  en: {
+    home: "Home",
+    about: "About",
+    projects: "Projects",
+    contact: "Contact",
+    download: "Download Resume",
+    switchLabel: "EN",
+    heroTitle: "I am Parth, <br /> Your Fullstack Developer",
+    heroDesc:
+      "I am a newly certified full-stack developer from Leipzig, Germany, holding a master's degree in Information Communication Systems. My passion for coding is driven by a fascination with creating solutions that are both technically robust and user-friendly, enhancing overall user satisfaction.",
+    heroResume: "Download Resume",
+    ContactMe: "Contact Me",
+    aboutTitle: "About Me",
+    aboutDesc:
+      "My passion for technology, combined with my experience in the tech industry and my master's studies, has enabled me to quickly acquire new skills. Below, I share how my journey has evolved, along with some of the activities I enjoy when I'm not working.",
+    backgroundTitle: "Background",
+    backgroundDesc:
+      "I've taken on diverse roles, seamlessly blending technical expertise with experience as an OB Engineer and a RADAR development student. My foundation in tech support and hardware simulation, along with a deep-rooted passion for technology, enables me to swiftly grasp and implement new skills 🎯.",
+    transitionTitle: "Transition",
+    transitionDesc:
+      "My time at Aptiv ⚡️ sparked my shift into development, where I discovered how to merge technical expertise with a user-focused approach. This role, combined with my customer-oriented background, highlighted the power of blending technology with user needs.",
+    downtimeTitle: "Downtime",
+    downtimeDesc:
+      "When I'm not coding, I love immersing myself in music, taking long drives, and tackling challenging puzzles with over 2,000 pieces. Each of these activities offers a unique way to unwind and stimulate my mind.",
+      skillsTitle: "Skills",
+      skillsDesc:
+        "I am a dynamic web developer proficient in both frontend and backend development, dedicated to crafting responsive and intuitive web applications. With expertise in a wide range of technologies and frameworks, I deliver secure, high-performing, and reliable solutions.",
+      frontendTitle: "Frontend Development",
+      frontendDesc:
+        "Specializing in creating responsive, user-friendly web applications using HTML, CSS, JavaScript, and TypeScript. Proficient in frameworks like React, Angular, and React Native.",
+      frontendCore: "Core Technologies:",
+      backendTitle: "Backend Development",
+      backendDesc:
+        "Proficient in server-side development with Node.js and Express, focusing on building robust APIs and managing databases using MongoDB and PostgreSQL.",
+      backendCore: "Core Technologies:",
+      additionalTitle: "Additional Skills",
+      additionalDesc:
+        "Experience with cloud storage solutions, local data storage, and advanced testing methodologies to ensure high-quality, secure applications.",
+      additionalCore: "Technologies & Tools:",
+      projectsTitle: "Projects",
+      projectsDesc:
+        'The <a href="https://careerfoundry.com/" target="_blank" rel="noopener">CareerFoundry</a> Full Stack Program catapulted me into the API realm, developing a backend API with Node.js as a base for clients in React and Angular. My journey expanded into mobile app development with React Native and crafting a Serverless App (PWA) using Test-Driven Development.',
+    pokemonTitle: "Pokemon Js App",
+    pokemonDesc:
+      "A JavaScript-based web app focused on Pokémon, utilizing HTML, CSS, and JavaScript to fetch and present detailed data from an external API.",
+    pokemonFeatures: "Features",
+    feature1: "Responsive Design",
+    feature2: "Fetch from API",
+    feature3: "Browse Pokemons",
+    pokemonTech: "Technologies",
+     myFlixDesc:
+      "A myFlix React App is a client-side, React-built platform offering detailed movie information. Users can create profiles, save favorites, and explore movies, part of a full-stack MERN application.",
+    accessMovieInfo: "Access Movie Information",
+    userAuth: "User Authentication",
+    manageFavorites: "Manage Favorites",
+    manageProfile: "Manage Profile Data",
+    reactNativeApp: "React Native App - A real-time chat app allowing users to exchange messages, share photos, videos, and their location instantly.",
+    sharePicsVideos: "Share Pics and Videos",
+    photoLibrary: "Photo Library Integration",
+    shareLocation: "Share Location",
+    realTimeChat: "Real-time Chat",
+    offlineAccess: "Offline Message Access",
+    angularApp: "This Angular app connects to a custom server-side REST API and database, offering access to movie details.",
+    userRegistration: "User Registration",
+    viewMovieDetails: "View Movie Details",
+    saveMovieFavorites: "Save Movie Favorites",
+    editUserProfile: "Edit User Profile",
+    serverlessApp:
+    "Created a serverless React PWA applying TDD practices, integrated with Google Calendar API for event retrieval.",
+    eventFiltering: "Event Filtering",
+    detailedEventViews: "Detailed Event Views",
+    offlineFunctionality: "Offline Functionality",
+    homeScreenShortcut: "Home Screen Shortcut",
+    dataVisualization: "Data Visualization",
+    myFlixServer: "A server-side component for myFlix, built with Node.js, Express, and MongoDB, offering a robust RESTful API for efficient movie data management.",
+    fetchMovies: "Fetch All Movies",
+    movieInfo: "Detailed Movie Info",
+    userProfile: "User Profile Management",
+    accountDeletion: "Account Deletion",
+    taskAppDescription: "A full-stack task management web app built with React, Node.js, and MongoDB, offering seamless organization and productivity features.",
+    taskFeature1: "Task Creation & Editing",
+    taskFeature2: "Due Date Reminders",
+    taskFeature3: "Priority Tagging",
+    taskFeature4: "Dark Mode Support",
+    contactHeader: "Contact Me 🤙",
+    contactText: "Open to joining creative ventures and projects. If you're in need of a committed collaborator, let's get in touch.",
+    resume: "src//downloads/PtDev.pdf",  // English resume file path
+  },
+
+
+  de: {
+    home: "Startseite",
+    about: "Über mich",
+    projects: "Projekte",
+    contact: "Kontakt",
+    download: "Lebenslauf herunterladen",
+    switchLabel: "DE",
+    heroTitle: "Ich bin Parth, <br /> Dein Fullstack-Entwickler",
+    heroDesc:
+      "Ich bin ein frisch zertifizierter Full-Stack-Entwickler aus Leipzig, Deutschland, mit einem Master-Abschluss in Informationskommunikationssystemen. Meine Leidenschaft für das Programmieren wird durch die Faszination an der Entwicklung technischer und benutzerfreundlicher Lösungen getrieben, die die Benutzerzufriedenheit verbessern.",
+    heroResume: "Lebenslauf herunterladen",
+    ContactMe: "Kontact",
+    aboutTitle: "Über mich",
+    aboutDesc:
+      "Meine Leidenschaft für Technologie, kombiniert mit meiner Erfahrung in der Tech-Branche und meinem Master-Studium, hat es mir ermöglicht, schnell neue Fähigkeiten zu erlernen. Im Folgenden teile ich meinen Werdegang sowie einige Aktivitäten, die ich in meiner Freizeit genieße.",
+    backgroundTitle: "Hintergrund",
+    backgroundDesc:
+      "Ich habe verschiedene Rollen übernommen und dabei technisches Know-how mit Erfahrung als OB-Ingenieur und RADAR-Entwicklungsstudent kombiniert. Meine Erfahrung im technischen Support und der Hardwaresimulation sowie meine tiefe Leidenschaft für Technologie ermöglichen es mir, neue Fähigkeiten schnell zu erlernen und umzusetzen 🎯.",
+    transitionTitle: "Übergang",
+    transitionDesc:
+      "Meine Zeit bei Aptiv ⚡️ weckte mein Interesse an der Entwicklung, wo ich lernte, technisches Fachwissen mit einem nutzerzentrierten Ansatz zu verbinden. Diese Rolle, kombiniert mit meinem kundenorientierten Hintergrund, verdeutlichte die Bedeutung der Verbindung von Technologie mit Benutzerbedürfnissen.",
+    downtimeTitle: "Freizeit",
+    downtimeDesc:
+      "Wenn ich nicht programmiere, tauche ich gerne in die Welt der Musik ein, mache lange Autofahrten und löse anspruchsvolle Puzzles mit über 2.000 Teilen. Jede dieser Aktivitäten bietet eine einzigartige Möglichkeit, sich zu entspannen und meinen Geist zu stimulieren.",
+      skillsTitle: "Fähigkeiten",
+      skillsDesc:
+        "Ich bin ein dynamischer Webentwickler, der sowohl im Frontend- als auch im Backend-Bereich versiert ist. Ich entwickle responsive und intuitive Webanwendungen mit einem breiten Spektrum an Technologien und Frameworks, um sichere, leistungsstarke und zuverlässige Lösungen bereitzustellen.",
+      frontendTitle: "Frontend-Entwicklung",
+      frontendDesc:
+        "Spezialisiert auf die Erstellung responsiver, benutzerfreundlicher Webanwendungen mit HTML, CSS, JavaScript und TypeScript. Erfahren in Frameworks wie React, Angular und React Native.",
+      frontendCore: "Kerntechnologien:",
+      backendTitle: "Backend-Entwicklung",
+      backendDesc:
+        "Erfahren in der serverseitigen Entwicklung mit Node.js und Express, mit Fokus auf robuste APIs und die Verwaltung von Datenbanken mit MongoDB und PostgreSQL.",
+      backendCore: "Kerntechnologien:",
+      additionalTitle: "Zusätzliche Fähigkeiten",
+      additionalDesc:
+        "Erfahrung mit Cloud-Speicherlösungen, lokaler Datenspeicherung und fortschrittlichen Testmethoden zur Gewährleistung qualitativ hochwertiger, sicherer Anwendungen.",
+      additionalCore: "Technologien & Werkzeuge:",
+      projectsTitle: "Projekte",
+    projectsDesc:
+      'Das <a href="https://careerfoundry.com/" target="_blank" rel="noopener">CareerFoundry</a> Full-Stack-Programm katapultierte mich in die Welt der APIs, wo ich eine Backend-API mit Node.js als Basis für Kunden in React und Angular entwickelte. Meine Reise erweiterte sich um die mobile App-Entwicklung mit React Native und die Erstellung einer serverlosen App (PWA) unter Verwendung von testgetriebener Entwicklung.',
+  pokemonTitle: "Pokemon Js App",
+    pokemonDesc:
+      "Eine auf JavaScript basierende Webanwendung mit Fokus auf Pokémon, die HTML, CSS und JavaScript verwendet, um detaillierte Daten von einer externen API abzurufen und anzuzeigen.",
+    pokemonFeatures: "Funktionen",
+    feature1: "Responsives Design",
+    feature2: "Abrufen von API",
+    feature3: "Pokémons durchsuchen",
+    pokemonTech: "Technologien",
+    myFlixDesc:
+    "Die myFlix React App ist eine clientseitige Plattform, die mit React erstellt wurde und detaillierte Filminformationen bietet. Benutzer können Profile erstellen, Favoriten speichern und Filme erkunden, als Teil einer Full-Stack-MERN-Anwendung.",
+    accessMovieInfo: "Filminformationen abrufen",
+    userAuth: "Benutzerauthentifizierung",
+    manageFavorites: "Favoriten verwalten",
+    manageProfile: "Profildaten verwalten",
+    reactNativeApp: "React Native App - Eine Echtzeit-Chat-App, die es Benutzern ermöglicht, Nachrichten auszutauschen, Fotos, Videos und ihren Standort sofort zu teilen.",
+    sharePicsVideos: "Bilder und Videos teilen",
+    photoLibrary: "Fotobibliothek-Integration",
+    shareLocation: "Standort teilen",
+    realTimeChat: "Echtzeit-Chat",
+    offlineAccess: "Offline-Nachrichtenzugriff",
+    angularApp: "Diese Angular-App verbindet sich mit einer benutzerdefinierten serverseitigen REST-API und Datenbank und bietet Zugriff auf Filmdetails.",
+    userRegistration: "Benutzerregistrierung",
+    viewMovieDetails: "Filmdetails anzeigen",
+    saveMovieFavorites: "Lieblingsfilme speichern",
+    editUserProfile: "Benutzerprofil bearbeiten",
+    serverlessApp:
+    "Erstellte eine serverlose React-PWA unter Anwendung von TDD-Praktiken, integriert mit der Google-Kalender-API zur Ereignisabfrage.",
+    eventFiltering: "Ereignisfilterung",
+    detailedEventViews: "Detaillierte Ereignisansichten",
+    offlineFunctionality: "Offline-Funktionalität",
+    homeScreenShortcut: "Startbildschirm-Verknüpfung",
+    dataVisualization: "Datenvisualisierung",
+    myFlixServer: "Eine serverseitige Komponente für myFlix, entwickelt mit Node.js, Express und MongoDB, die eine robuste RESTful-API für effizientes Filmdatenmanagement bietet.",
+    fetchMovies: "Alle Filme abrufen",
+    movieInfo: "Detaillierte Filminfos",
+    userProfile: "Benutzerprofil Verwaltung",
+    accountDeletion: "Konto löschen",
+    taskAppDescription: "Eine Full-Stack-Aufgabenverwaltungs-Webanwendung, die mit React, Node.js und MongoDB entwickelt wurde und nahtlose Organisations- und Produktivitätsfunktionen bietet.",
+    taskFeature1: "Aufgabenerstellung & Bearbeitung",
+    taskFeature2: "Erinnerungen an Fälligkeitsdaten",
+    taskFeature3: "Prioritätenkennzeichnung",
+    taskFeature4: "Unterstützung für den Dunkelmodus",
+    contactHeader: "Kontaktiere mich 🤙",
+    contactText: "Ich bin offen für kreative Unternehmungen und Projekte. Wenn du einen engagierten Mitstreiter suchst, lass uns in Kontakt treten.",
+    resume: "src/downloads/Resume_German.pdf",  // German resume file path
+  }
+};
+
+
+function changeLanguage(lang) {
+  localStorage.setItem("selectedLanguage", lang); // Save preference
+
+  // Update text content based on language
+  document.querySelectorAll("[data-lang]").forEach((element) => {
+    let key = element.getAttribute("data-lang");
+    element.innerHTML = translations[lang][key]; // Use innerHTML for <br /> support
+  });
+
+  // Update the button label
+  document.getElementById("lang-toggle").innerText = translations[lang].switchLabel;
+}
+
+// function updateLanguage(lang) {
+//   // Update text content based on language
+//   document.querySelectorAll("[data-lang]").forEach((element) => {
+//     const key = element.getAttribute("data-lang");
+//     if (translations[lang][key]) {
+//       element.textContent = translations[lang][key]; // Update text content
+//     }
+//   });
+
+//   // Update the download link based on language
+//   const downloadLink = document.getElementById("downloadResume");
+//   if (translations[lang].resume) {
+//     downloadLink.href = translations[lang].resume;  // Update resume link
+//   }
+// }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const savedLang = localStorage.getItem("selectedLanguage") || "en";
+  changeLanguage(savedLang);
+
+  document.getElementById("lang-toggle").addEventListener("click", function () {
+    let currentLang = localStorage.getItem("selectedLanguage") || "en";
+    let newLang = currentLang === "en" ? "de" : "en";
+    changeLanguage(newLang);
+  });
+});
